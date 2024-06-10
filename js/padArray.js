b670ed545748bd574145043597a37fb912c466b8
robertuptc <robertuptc@gmail.com>
2024-06-10 12:31:14 -0400

    js problem finished

--- js/padArray.js
+++ js/padArray.js
@@ -1,6 +1,13 @@
-// REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
-    // code goes here : ) 
+export const pad = (array, minSize, value=null) => {
+    return minSize <= array.length ? array: padding(array, minSize, value)
 }
 
-exports.pad = pad;
+function padding(array, minSize, value) {
+    let startAt = array.length
+    for (let i = startAt; i < minSize; i++) {
+        value == null ? array[i] = null : array[i] = value
+    }
+    return array
+}
+
+
