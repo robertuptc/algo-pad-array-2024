f4a0e8be5f4ef4a68a3ccb7dbe9cd8fb73a3468a
robertuptc <robertuptc@gmail.com>
2024-06-10 12:58:50 -0400

    python problem finished

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,3 +1,9 @@
-#REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
-    pass 
+    return array if value == 0 else padding(array, min_size, value)
+
+
+def padding(array, min_zise, value):
+    start_at = len(array)
+    for i in range(start_at, min_zise):
+        array.append(value)
+    return array
